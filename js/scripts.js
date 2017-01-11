2ab797f2ba92d8d0cc5ad4dcf3e4d29cd2934e7e
Keely Glenn and Matt Caswell <keely@>
2017-01-11 11:18:15 -0800

    add bones of html with links to javascripts and css

--- js/scripts.js
+++ js/scripts.js
@@ -9,35 +9,35 @@ var addResult = add(number1, number2);
 
 alert(addResult);
 
-// For Subtraction
-var sub = function(number1, number2) {
-	return number1 - number2;
-};
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var subResult = sub(number1, number2);
-
-alert(subResult);
-
-// For Multiplication
-var mult = function(number1, number2) {
-	return number1 * number2;
-};
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var multResult = mult(number1, number2);
-
-alert(multResult);
-
-// For Division
-var div = function(number1, number2) {
-	return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var divResult = div(number1, number2);
-
-alert(divResult);
+// // For Subtraction
+// var sub = function(number1, number2) {
+// 	return number1 - number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+// var subResult = sub(number1, number2);
+//
+// alert(subResult);
+//
+// // For Multiplication
+// var mult = function(number1, number2) {
+// 	return number1 * number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+// var multResult = mult(number1, number2);
+//
+// alert(multResult);
+//
+// // For Division
+// var div = function(number1, number2) {
+// 	return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+// var divResult = div(number1, number2);
+//
+// alert(divResult);
